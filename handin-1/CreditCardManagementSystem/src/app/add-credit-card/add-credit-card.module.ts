c90cacba6959b7629ee946e71f7fb077806c520c
phil2988 <phillipblicher@gmail.com>
2022-09-06 21:09:13 +0200

    added form controls and validators

--- handin-1/CreditCardManagementSystem/src/app/add-credit-card/add-credit-card.module.ts
+++ handin-1/CreditCardManagementSystem/src/app/add-credit-card/add-credit-card.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AddCreditCardPageComponent } from './add-credit-card-page/add-credit-card-page.component';
 import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
--- handin-1/CreditCardManagementSystem/src/app/add-credit-card/credit-card-form/credit-card-form.component.ts
+++ handin-1/CreditCardManagementSystem/src/app/add-credit-card/credit-card-form/credit-card-form.component.ts
@@ -1,34 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-card-form',
   templateUrl: './credit-card-form.component.html',
   styleUrls: ['./credit-card-form.component.css']
 })
-export class CreditCardFormComponent implements OnInit {
-
+export class CreditCardFormComponent {
   creditCardForm: FormGroup;
+  expirationDate: FormGroup;
 
   constructor(){
+
+    this.expirationDate = new FormGroup({
+      month: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.max(12),
+        Validators.min(1)
+      ]),
+      year: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.max(31),
+        Validators.min(1)
+      ]),
+    });
+
     this.creditCardForm = new FormGroup({
-      cardHolder: new FormControl(''),
-      cardNumber: new FormControl(''),
-      cscCode: new FormControl(''),
-      cardIssuer: new FormControl(''),
-      expirationDate: new FormGroup({
-        month: new FormControl(''),
-        year: new FormControl(''),
-      }),
+      cardHolder: new FormControl('', [
+        Validators.required, 
+        Validators.maxLength(30)
+      ]),
+      cardNumber: new FormControl('', [
+        Validators.required, 
+        Validators.pattern("^[0-9]*$")
+      ]),
+      cscCode: new FormControl('', [
+        Validators.required, 
+        Validators.pattern("^[0-9]*$"),
+        Validators.min(100),
+        Validators.max(999)
+      ]),
+      cardIssuer: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30)
+      ]),
+      expirationDate: this.expirationDate
     });
   }
 
-  ngOnInit(): void {
-  }
-
   submitCard(){
-    alert(
-      "Hello " + this.creditCardForm.controls['cardHolder'].value +
-      "! with card number " + this.creditCardForm.controls['cardNumber'].value)
+
   }
 }
