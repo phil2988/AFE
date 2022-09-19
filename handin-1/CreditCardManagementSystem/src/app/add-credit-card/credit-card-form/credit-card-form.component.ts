aa01db1e41e117687bd55130b7600dafd77017d8
phil2988 <phillipblicher@gmail.com>
2022-09-19 10:36:21 +0200

    merging pc with desktop

--- handin-1/CreditCardManagementSystem/src/app/add-credit-card/credit-card-form/credit-card-form.component.ts
+++ handin-1/CreditCardManagementSystem/src/app/add-credit-card/credit-card-form/credit-card-form.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, pipe } from 'rxjs';
-import { AppService } from 'src/app/app.service';
-import { CreditCard } from 'src/app/entities/credit-card';
+
 @Component({
   selector: 'app-credit-card-form',
   templateUrl: './credit-card-form.component.html',
@@ -17,15 +15,15 @@ export class CreditCardFormComponent {
 
     this.creditCardForm = new FormGroup({
       cardHolder: new FormControl('', [
-        Validators.required,
+        Validators.required, 
         Validators.maxLength(30)
       ]),
       cardNumber: new FormControl('', [
-        Validators.required,
+        Validators.required, 
         Validators.pattern("^[0-9]*$")
       ]),
       cscCode: new FormControl('', [
-        Validators.required,
+        Validators.required, 
         Validators.pattern("^[0-9]*$"),
         Validators.min(100),
         Validators.max(999)
@@ -49,20 +47,9 @@ export class CreditCardFormComponent {
         ]),
       })
     });
-
-    this.submitCard();
   }
 
   submitCard(){
-    this.creditCard = {
-      card_number: this.creditCardForm.controls['cardNumber'].value,
-      cardholder_name: this.creditCardForm.controls['cardHolder'].value,
-      csc_code: this.creditCardForm.controls['cscCode'].value,
-      expiration_date_month: this.creditCardForm.controls['expirationDate'].value.month,
-      expiration_date_year: this.creditCardForm.controls['expirationDate'].value.year,
-      issuer: this.creditCardForm.controls['cardIssuer'].value
-    };
-
-    const response = this.appService.sendApiRequest('POST', "credit_cards", this.creditCard)
+    console.log(this.creditCardForm)
   }
 }
