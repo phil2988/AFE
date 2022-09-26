e0d582cdffc791420113f7c3586972db6ab43b0e
phil2988 <phillipblicher@gmail.com>
2022-09-26 12:17:06 +0100

    s

--- handin-1/CreditCardManagementSystem/src/app/app.module.ts
+++ handin-1/CreditCardManagementSystem/src/app/app.module.ts
@@ -4,17 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import { NavigationComponent } from './navigation/navigation.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
 import { AddCreditCardModule } from './add-credit-card/add-credit-card.module';
-import { HomeScreenModule } from './home-screen/home-screen.module';
-import { NavigationComponent } from './navigation/navigation.component';
 import { AppService } from './app.service';
 import { CreditCardDetailsModule } from './credit-card-details/credit-card-details.module';
+import { HomeScreenModule } from './home-screen/home-screen.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
